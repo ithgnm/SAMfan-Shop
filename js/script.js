d8064905b4371a75f471b0115ad4609cb7d3864e
Mojave <mojave@Mojaves-MacBook-Pro.local>
2019-01-15 16:23:20 +0700

    Update JS

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
 	$('button[id=remove]').click(function() {
 		$(this).parent().parent().hide();
 	});
+	$(window).on("scroll", function() {
+        if($(window).scrollTop() > 90) 
+        	$('#nav-index').addClass('bg-dark fixed-top');
+        else $('#nav-index').removeClass('bg-dark fixed-top');
+  	});
+  	$('#index-button').click(function() {
+		$('#nav-index').addClass('bg-dark');
+	});
 });
